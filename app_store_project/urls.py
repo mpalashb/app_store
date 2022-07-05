cb5c7510381dd9063a47816bc8f4643462d5b7e2
mpalashb <mdpalashbabu4@gmail.com>
2022-07-06 01:42:29 +0600

    changed production settings

--- app_store_project/urls.py
+++ app_store_project/urls.py
@@ -11,5 +11,8 @@ urlpatterns = [
     # path('v2/', include('users.urls')),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
