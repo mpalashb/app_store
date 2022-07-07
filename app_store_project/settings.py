41bf74a5428c714d25f891ad24f7bd0f3875a6b1
Mohammad Palash Babu <mdpalashbabu4@gmail.com>
2022-07-07 15:48:58 +0600

    added for public repo

--- app_store_project/settings.py
+++ app_store_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from datetime import timedelta
 
 from pathlib import Path
@@ -29,7 +30,7 @@ SECRET_KEY = 'django-insecure-=_1i6a^)2cmyicd10v1#w8(lizr*q4u5s5=7tj3w-7f-^6)pt3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['104.248.195.133']
+ALLOWED_HOSTS = ['myserverip']
 
 
 # Application definition
@@ -164,13 +165,12 @@ MESSAGE_TAGS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-
-EMAIL_HOST = None
-EMAIL_HOST_USER = None
-EMAIL_HOST_PASSWORD = None
-EMAIL_PORT = None
-EMAIL_USE_TLS = None
-DEFAULT_FROM_EMAIL = None
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 
 STATIC_ROOT = BASE_DIR / "static"
 # MEDIA_ROOT = 'uploads'
